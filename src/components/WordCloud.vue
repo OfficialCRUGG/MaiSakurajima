d226939fab524d685d16b913a1083e082ab8da42
OfficialCRUGG <ich@crugg.de>
2021-08-03 19:04:14 +0200

    ✨ Made words in Cloud more interactive

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="text-white bg-dark-800 p-10 text-center">
-        <h1 class="font-bold text-4xl mt-10">Just a small selection of adjectives to describe her</h1>
+        <h1 class="font-bold text-4xl mt-10">{{ $t("home.wordCloud.title") }}</h1>
+        <h2 class="font-semibold text-3xl mt-3">{{ $t("home.wordCloud.subtitle") }}</h2>
         <vue-word-cloud class="max-w-6xl bg-dark-900 rounded-2xl mx-auto m-10" style="height: 600px; width: 100%;" :words="words">
             <template slot-scope="{text}">
-                <a :href="`${ $t('home.wordCloud.dicLink', [text]) }`" target="__blank" class="text-white text-5xl font-sans font-semibold">{{text}}</a>
+                <a :href="`${ $t('home.wordCloud.dicLink', [text]) }`" target="__blank" class="text-white text-5xl font-sans font-semibold word">{{text}}</a>
             </template>
         </vue-word-cloud>
     </div>
@@ -32,3 +33,16 @@ export default Vue.extend({
     }
 })
 </script>
+
+<style scoped>
+.word {
+    opacity: 80%;
+    text-shadow: 0 0 0px rgba(255,255,255,0.5);
+    transition-duration: .5s;
+}
+
+.word:hover {
+    opacity: 100%;
+    text-shadow: 0 0 40px rgba(255,255,255,0.5);
+}
+</style>
